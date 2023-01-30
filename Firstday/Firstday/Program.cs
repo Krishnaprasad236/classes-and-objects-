f3fdc92eb24bc62920ec75e0c64d588afc4c39fa
Krishnaprasad236 <addanki.krishnaprasad@graniteriverlabs.in>
2023-01-30 18:34:36 +0530

    Classes and objects
    creation of classes and objects

---
+++ Firstday/Firstday/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firstday
+{
+    internal class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("enter first number :");
+            int a=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter second number :");
+            int b=Convert.ToInt32(Console.ReadLine());
+
+            Addition sum=new Addition();
+            Console.WriteLine("sum of two number : "+sum.sum(a, b));
+
+            Substarction substarction=new Substarction();
+            
+            Console.WriteLine("substraction of two number :"+substarction.sub(a, b));
+
+            Multiplication multiplication=new Multiplication(); 
+            Console.WriteLine("multiplcation of two numbers :"+multiplication.mul(a, b));
+           
+
+            Division division=new Division();
+            Console.WriteLine("dividion of two numbers :"+division.div(a,b));
+
+        }
+    }
+}
